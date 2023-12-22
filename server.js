927ce56832fff63d2078ecd5fb9c4ad3bb7e03dc
Hamza Zaher <hamza.zaher12@gmail.com>
2023-12-22 18:20:55 +0300

    get all quotes api, get quote by person and post a new quote api

--- server.js
+++ server.js
@@ -1,10 +1,59 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const { quotes } = require('./data');
-const { getRandomElement } = require('./utils');
+const { quotes } = require("./data");
+const { getRandomElement } = require("./utils");
 
 const PORT = process.env.PORT || 4001;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+app.get("/api/quotes/random", (req, res, next) => {
+  const randomQuote = getRandomElement(quotes);
+
+  res.send({
+    quote: randomQuote,
+  });
+});
+
+app.get("/api/quotes", (req, res, next) => {
+  const queryParams = req.query;
+  console.log(req.query);
+
+  if (queryParams.person) {
+    const quotesByPerson = quotes.filter(
+      (quote) => quote.person.toLowerCase() === queryParams.person.toLowerCase()
+    );
+    res.send({
+      quotes: quotesByPerson,
+    });
+  } else {
+    res.send({
+      quotes: quotes,
+    });
+  }
+});
+
+app.post("/api/quotes", (req, res, next) => {
+  console.log(req.query);
+  const newQuote = req.query.quote;
+  const newPerson = req.query.person;
+  if (newQuote && newPerson) {
+    const newQuoteObj = {
+      quote: newQuote,
+      person: newPerson,
+    };
+    quotes.push(newQuoteObj);
+    res.send({
+      quote: quotes.find(
+        (quote) => quote.person === newPerson && quote.quote === newQuote
+      )
+    });
+  } else {
+    res.status(400).send();
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+});
